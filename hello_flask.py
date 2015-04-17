975785b575068cb3c7e56ebfe01fcd610f8d7c64
liuminL <894993476@qq.com>
2015-04-17 13:23:32 +0800

    the flask

--- hello_flask.py
+++ hello_flask.py
@@ -6,4 +6,12 @@ app = Flask('my_web_app')
 def hello():
     return '<h1>Hello Flask</h1>'
 
+@app.route('/sayhello')
+def  say_hello():
+	return 'Say hello'
+
+@app.route('/sayhelloto/<name>')
+def say_hello_to(name):
+	return 'Hello, ' + name
+
 app.run()
