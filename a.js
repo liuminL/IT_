12046c4215e634064db8d4d49a73bd27ddf5a93d
liuminL <894993476@qq.com>
2015-04-27 21:21:39 +0800

    change

--- a.js
+++
@@ -1,6 +0,0 @@
-var tes = require("./fun_test.js");
-
-tes.test_of_mul();
-tes.test_of_add();
-tes.test_of_div();
-tes.test_of_sub();
--- index.js
+++
@@ -1,3 +0,0 @@
-var serve = require("./serve");
-
-serve.start();
