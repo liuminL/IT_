499cbbb039c0ecdc1b85d4af1a08ac78f1a52833
liuminL <894993476@qq.com>
2015-05-21 21:07:17 +0800

    abort js

---
+++ jsTry/js2/test1.js
@@ -0,0 +1,13 @@
+/*document.write("<h1>标签h1</h1>");//output
+document.getElementById('pid').innerHTML="hellohello";
+var arr = new Array("nihao", "good", "nihao" );
+document.write(arr[2]);
+document.write(arr[1]);
+alert(arr[1]);
+
+function demo(){
+	var a = 10;
+	var b = 10;
+	var sun = a + b;
+	alert(sun);
+}*/
